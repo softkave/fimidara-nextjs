fe3aa84e030999510f9af6685b3b6c285c4f9842
Abayomi Akintomide <ywordk@gmail.com>
2022-05-04 18:55:44 -0400

    Another attempt at fixing font issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,24 @@ import store from "../lib/store/store";
 import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 import "../styles/antd.less";
+import Head from "next/head";
 
 function FilesApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
+          rel="stylesheet"
+        />
+        <title>fimidara</title>
+      </Head>
       <NextNProgress />
       <Component {...pageProps} />
     </Provider>
