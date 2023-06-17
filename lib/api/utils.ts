8fd019c2388c1ec2a2eaf947c0b5f991f7d87e1d
Abayomi Akintomide <ywordk@gmail.com>
2023-06-17 12:53:55 -0400

    Copied over FimidaraEndpointError changes

--- lib/api/utils.ts
+++ lib/api/utils.ts
@@ -22,6 +22,7 @@ type FimidaraEndpointErrorItem = {
 
 export class FimidaraEndpointError extends Error {
   name = 'FimidaraEndpointError';
+  isFimidaraEndpointError = true;
 
   constructor(
     public errors: Array<FimidaraEndpointErrorItem>,
