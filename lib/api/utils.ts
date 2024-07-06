2da57c706395b52f09a6b9478942c212a5b884ff
Abayomi Akintomide <ywordk@gmail.com>
2024-07-06 13:55:30 -0400

    feat: upgrade fimidara js version

--- lib/api/utils.ts
+++ lib/api/utils.ts
@@ -80,6 +80,14 @@ export class FimidaraJsConfig {
   }
 }
 
+function isNodeEnv() {
+  return typeof window === 'undefined' && typeof process === 'object';
+}
+
+// function isBrowserEnv() {
+//   return !isNodeEnv();
+// }
+
 const HTTP_HEADER_CONTENT_TYPE = 'content-type';
 const HTTP_HEADER_CONTENT_LENGTH = 'Content-Length';
 const HTTP_HEADER_AUTHORIZATION = 'authorization';
@@ -123,7 +131,10 @@ export async function invokeEndpoint(props: InvokeEndpointParams) {
   } else if (data) {
     const str = JSON.stringify(data);
     contentBody = str;
-    outgoingHeaders[HTTP_HEADER_CONTENT_TYPE] = CONTENT_TYPE_APPLICATION_JSON;
+
+    if (isNodeEnv()) {
+      outgoingHeaders[HTTP_HEADER_CONTENT_TYPE] = CONTENT_TYPE_APPLICATION_JSON;
+    }
 
     if (
       !outgoingHeaders[HTTP_HEADER_CONTENT_LENGTH] ||
