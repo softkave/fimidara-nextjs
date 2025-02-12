82d62f8403119abb22ae270242afd7f9958587cb
Abayomi Akintomide <ywordk@gmail.com>
2025-02-12 03:03:58 -0500

    added error page

---
+++ app/error/page.tsx
@@ -0,0 +1,27 @@
+import { DefaultErrorMessage } from "@/components/message/default-error-message.tsx";
+import { ErrorTypeMessage } from "@/components/message/error-type-message.tsx";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "fimidara",
+  description: "Something went wrong",
+};
+
+export default function AuthErrorPage() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <a
+        href="#"
+        className="block max-w-sm bg-white p-6 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 flex flex-row items-center gap-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Something went wrong
+        </h5>
+        <Suspense fallback={<DefaultErrorMessage />}>
+          <ErrorTypeMessage />
+        </Suspense>
+      </a>
+    </div>
+  );
+}
