0bde3755dbe81462c75b486a84647671575b7f3f
Abayomi Akintomide <ywordk@gmail.com>
2025-02-16 12:17:14 -0500

    debugging blank logged in page

--- components/hooks/usePageAuthRequired.tsx
+++ components/hooks/usePageAuthRequired.tsx
@@ -68,6 +68,7 @@ export const usePageAuthRequired = (options: WithPageAuthRequiredOptions) => {
       trackedLoggedIn.state === false &&
       !isRedirectingToLogin.current
     ) {
+      console.log("redirecting to login");
       isRedirectingToLogin.current = true;
       const returnToPath = returnTo || pathname;
       const loginPath = kAppAccountPaths.loginWithReturnPath(returnToPath);
